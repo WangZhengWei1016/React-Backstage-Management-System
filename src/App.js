ac2feef11f481ca61d4c8cbbb70f2171d733423d
wzw <1435844429@qq.com>
2019-12-17 23:55:10 +0800

    import antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React,{Component} from 'react'
+import {Button, message} from 'antd'
 
 export default class App extends Component {
+
+    handleClick = () => {
+        message.success('成功啦...')
+    }
+
     render() {
         return (
-            <div>App</div>
+            <Button onClick={this.handleClick}>提交</Button>
         )
     }
 }
