8e185fe0cc9392b20818ce3d04beaa28ab7792cb
wzw <1435844429@qq.com>
2019-12-25 15:10:57 +0800

    admin base route

---
+++ src/components/LeftNav/LeftNav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import './LeftNav.less'
+
+
+export default class LeftNav extends Component {
+    render() {
+        return (
+            <div className='left-nav'>
+                Sider
+            </div>
+        )
+    }
+}
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
-import {Redirect} from 'react-router-dom'
+import { Redirect, Switch, Route } from 'react-router-dom'
+import { Layout } from "antd";
 
 import memoryUtils from '../../utils/memoryUtils'
+import LeftNav from '../../components/LeftNav/LeftNav'
+import Header from '../../components/Header/Header'
+import Home from '../Home/Home'
+import Category from '../Category/Category'
+import Product from '../Product/Product'
+import User from '../User/User'
+import Role from '../Role/Role'
+import Bar from '../Charts/Bar'
+import Line from '../Charts/Line'
+import Pie from '../Charts/Pie'
+
+const { Sider, Content, Footer } = Layout
 
 export default class Admin extends Component {
     render() {
@@ -14,9 +27,28 @@ export default class Admin extends Component {
         }
 
         return (
-            <div>
-                HELLO，{user.username}
-            </div>
+            <Layout style={{'height': '100%'}}>
+                <Sider>
+                    <LeftNav />
+                </Sider>
+                <Layout>
+                    <Header />
+                    <Content style={{'background': 'white'}}>
+                        <Switch>
+                            <Route path='/home' component={Home} />
+                            <Route path='/category' component={Category} />
+                            <Route path='/product' component={Product} />
+                            <Route path='/user' component={User} />
+                            <Route path='/role' component={Role} />
+                            <Route path='/charts/bar' component={Bar} />
+                            <Route path='/charts/line' component={Line} />
+                            <Route path='/charts/pie' component={Pie} />
+                            <Redirect to='/home' />
+                        </Switch>
+                    </Content>
+                    <Footer style={{'textAlign': 'center', 'color': '#999'}}>推荐使用Chrome浏览器，可以获得更佳页面操作体验</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
